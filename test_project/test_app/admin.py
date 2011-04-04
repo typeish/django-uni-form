0085a0da3d5568c0f1b609cf89e242584ad4ec43
John Debs <johnthedebs@gmail.com>
2011-04-04 00:31:40 -0400

    Override Django admin's change form template with our own copy

--- test_project/test_app/admin.py
+++ test_project/test_app/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from test_app.models import TestModel
 
 class TestModelAdmin(admin.ModelAdmin):
+    change_form_template = "test_app/change_form.html"
+
     fieldsets = [
         ("First", {
             "classes": ("first", ),
